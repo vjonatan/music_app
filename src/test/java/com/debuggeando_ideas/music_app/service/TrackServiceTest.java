2f0243f317da4aaea1b5e2fecaa448d2840cbfbe
Jonatan <jonatan.e.vera@gmail.com>
2024-05-12 16:39:03 -0300

    Test getAll, save, delete, findById y update

--- src/test/java/com/debuggeando_ideas/music_app/service/TrackServiceTest.java
+++ src/test/java/com/debuggeando_ideas/music_app/service/TrackServiceTest.java
@@ -1,6 +1,7 @@
 package com.debuggeando_ideas.music_app.service;
 
 import com.debuggeando_ideas.music_app.*;
+import com.debuggeando_ideas.music_app.entity.*;
 import com.debuggeando_ideas.music_app.repository.*;
 import com.debuggeando_ideas.music_app.service.impl.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.*;
 import org.springframework.test.context.*;
 
+import javax.xml.crypto.*;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,5 +49,63 @@ public class TrackServiceTest {
                 () -> trackService.findById(INVALID_ID)
         );
 
+    }
+
+    @Test
+    void getAll(){
+        var expected = Set.of(DataDummy.TRACK_1, DataDummy.TRACK_2);
+        when(trackRepository.findAll())
+                .thenReturn(expected);
+        var result = trackService.getAll();
+
+        assertEquals(2, result.size());
+        assertEquals(expected, result);
+        ;
+    }
+
+    @Test
+    void save(){
+        this.trackService.save(DataDummy.TRACK_1);
+        verify(trackRepository, times(1)).save(any(TrackEntity.class));
+    }
+
+    @Test
+    void findById(){
+        when(trackRepository.findById(VALID_ID))
+                .thenReturn(Optional.of(DataDummy.TRACK_1));
+        TrackEntity result = this.trackService.findById(VALID_ID);
+        assertEquals(DataDummy.TRACK_1, result);
+        assertThrows(NoSuchElementException.class,
+                () -> this.trackService.findById(INVALID_ID));
+    }
+
+    @Test
+    void delete() {
+        this.trackService.delete(VALID_ID);
+        verify(this.trackRepository, times(1)).deleteById(eq(VALID_ID));
+    }
+
+    @Test
+    void update() {
+        when(this.trackRepository.existsById(VALID_ID))
+                .thenReturn(true);
+
+        when(this.trackRepository.existsById(INVALID_ID))
+                .thenReturn(false);
+
+        when(this.trackRepository.findById(VALID_ID))
+                .thenReturn(Optional.of(DataDummy.TRACK_1));
+
+        when(this.trackRepository.save(any(TrackEntity.class)))
+                .thenReturn(DataDummy.TRACK_2);
+
+        TrackEntity result = this.trackService.update(DataDummy.TRACK_1, VALID_ID);
+
+        assertEquals(DataDummy.TRACK_2, result);
+        assertThrows(
+                NoSuchElementException.class,
+                () -> trackService.update(DataDummy.TRACK_1, INVALID_ID)
+        );
+        verify(this.trackRepository, times(2)).existsById(any(Long.class));
     }
 }
