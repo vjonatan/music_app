0ea0b8d8f3a20fe5704af5a09ae86c66c6c403e9
Jonatan <jonatan.e.vera@gmail.com>
2024-07-27 16:37:21 -0300

    - AlbumControllerTest: se agrega test save() y update()

--- src/test/java/com/debuggeando_ideas/music_app/controller/AlbumControllerTest.java
+++ src/test/java/com/debuggeando_ideas/music_app/controller/AlbumControllerTest.java
@@ -1,13 +1,17 @@
 package com.debuggeando_ideas.music_app.controller;
 
 import com.debuggeando_ideas.music_app.*;
+import com.debuggeando_ideas.music_app.dto.*;
 import com.debuggeando_ideas.music_app.service.*;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.http.*;
 import org.springframework.test.web.servlet.*;
+
+import javax.xml.crypto.*;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -36,6 +40,9 @@ public class AlbumControllerTest extends AlbumControllerSpec {
     void setupMocks(){
         when(this.albumServiceMock.findById(eq(VALID_ID)))
                 .thenReturn(DataDummy.ALBUM_DTO);
+
+        when(this.albumServiceMock.save(eq(DataDummy.ALBUM_DTO)))
+                .thenReturn(DataDummy.ALBUM_DTO);
     }
 
     @Test
@@ -49,5 +56,35 @@ public class AlbumControllerTest extends AlbumControllerSpec {
                 .andExpect(jsonPath("$.name").value(DataDummy.ALBUM_DTO.getName()))
                 .andExpect(jsonPath("$.autor").value(DataDummy.ALBUM_DTO.getAutor()))
                 .andExpect(jsonPath("$.price").value(DataDummy.ALBUM_DTO.getPrice()));
+    }
+
+    @Test
+    void save() throws Exception {
+        mockMvc.perform(
+                post(RESOURCE_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(DataDummy.ALBUM_DTO))
+                )
+                .andExpect(status().isCreated());
+        verify(this.albumServiceMock).save(eq(DataDummy.ALBUM_DTO));
+    }
+
+    @Test
+    void update() throws Exception {
+        var uri = RESOURCE_PATH + "/" + VALID_ID;
+        final var albumToUpdate = DataDummy.ALBUM_DTO;
+        albumToUpdate.setName("Los Playeros");
+
+        when(this.albumServiceMock.update(eq(albumToUpdate), eq(VALID_ID)))
+                .thenReturn(albumToUpdate);
+
+        mockMvc.perform(
+                put(uri).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(albumToUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("Los Playeros"));
+
+        verify(this.albumServiceMock).update(eq(DataDummy.ALBUM_DTO), eq(VALID_ID));
     }
 }
